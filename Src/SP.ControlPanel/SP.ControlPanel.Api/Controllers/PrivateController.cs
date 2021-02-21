1d84b3db11c33e05549bb2507f0d5474c9cc68f8
Jessica Lugo <22513463+SoftwarePony@users.noreply.github.com>
2021-02-21 09:43:29 -0800

    Tests Authentication and Authorization

---
+++ Src/SP.ControlPanel/SP.ControlPanel.Api/Controllers/PrivateController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SP.ControlPanel.Api.Controllers
+{
+    [Authorize(Policy = "IsGlobalAdministrator")]
+    [Route("[controller]")]
+    [ApiController]
+    public class PrivateController : Controller
+    {
+        public IActionResult Get()
+        {
+            return Ok("Hello World!");
+        }
+    }
+}
---
+++ Src/SP.ControlPanel/SP.ControlPanel.Api/Controllers/PublicController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SP.ControlPanel.Api.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class PublicController : Controller
+    {
+        public IActionResult Get()
+        {
+            return Ok("Hello World!");
+        }
+    }
+}
